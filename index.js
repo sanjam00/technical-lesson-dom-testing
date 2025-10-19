f54b2a36d3a761824e70ed50c32f693614524e45
Sanaya James <sanaya.j.james@gmail.com>
2025-10-19 12:40:03 -0500

    inserted code into index.js and index.test.js

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Your code here!
+function addItem() {
+    //select the <ul> element by ID
+    const list = document.getElementById("item-list");
+    // Create a new <li> element
+    const newItem = document.createElement("li");
+
+    // Set the text content of the new <li>
+    newItem.textContent = "New Item";
+
+    // Append the new <li> to the <ul>
+    list.appendChild(newItem);
+}
+
+// Add a click event listener to the button with the ID 'add-item' after DOMContentLoaded
+// When clicked, it will call the addItem function
+document.addEventListener("DOMContentLoaded", () => {
+ document.getElementById("add-item").addEventListener("click", addItem);
+})
--- test/index.test.js
+++ test/index.test.js
@@ -2,4 +2,45 @@
  * @jest-environment jsdom
  */
 
-// Your code here!
+const fs = require("fs");
+const path = require("path");
+
+//load the html file into jsdom
+const html = fs.readFileSync(path.resolve("index.html"), "utf-8");
+
+let dom;
+let document;
+
+beforeAll(() => {
+    document.body.innerHTML = html;
+
+    //Load and run the script once
+    require("../index.js");
+
+    //Fire DOMContentLoaded once so the event listener gets attached
+    document.displatchEvent(new Event("DOMContentLoaded"));
+})
+
+beforeEach(() => {
+    //Clear list for each test
+    document.getElementByID("item-list").innerHTML = "";
+});
+
+describe("Add Item Button", () => {
+    test("add a new item to the list when the button is clicked", () => {
+        //grab add item button and list where items are displayed
+        const button = document.getElementByID("add-item");
+        const list = document.getElementByID("item-list");
+
+        //check the list is initially empty
+        expect(list.children.length).toBe(0);
+
+        //simulate button click
+        button.click();
+
+        //Get all the <li> elements in the list and verify the count and text
+        expect(list.children.length).toBe(1);
+
+        expect(list.children[0].textContent).toBe("New Item");
+    })
+})
